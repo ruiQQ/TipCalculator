f0bb9b4d7f2333bd23b7993cdc79dce3d5c811e0
Rui <ruiqiu15@gmail.com>
2017-12-30 01:12:11 -0800

    working version with 3 tip options

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    //@IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        // tap anywhere to hide keyboard and end editing
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.1, 0.15, 0.2]
+        
+        // convert string into a number, if nil, 0
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+    }
+    
 }
 
